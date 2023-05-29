681e85112ea671fb4aa351ca10ec710ba29bbda6
wglee0511 <ranazang@naver.com>
2023-05-29 17:03:42 +0900

    chore: 나머지 부분 업데이트

---
+++ ReactotronConfig.js
@@ -0,0 +1,48 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { NativeModules } from 'react-native';
+import Reactotron, { asyncStorage, networking } from 'reactotron-react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// First, set some configuration settings on how to connect to the app
+Reactotron.configure({
+  name: 'Payhere Front',
+});
+
+// add every built-in react native feature.  you also have the ability to pass
+// an object as a parameter to configure each individual react-native plugin
+// if you'd like.
+Reactotron.useReactNative({
+  asyncStorage: { ignore: ['secret'] },
+  networking: {
+    ignoreUrls: /https:\/\/clients3.google.com\/generate_204/,
+  },
+});
+
+// add some more plugins for redux & redux-saga
+Reactotron.use(asyncStorage(), networking());
+
+// for ease of debugging, wrap console.log with reactotron.log
+const goodOldConsoleLog = console.log;
+console.log = (...args) => {
+  goodOldConsoleLog(...args);
+  Reactotron.log(...args);
+};
+
+let scriptHostname;
+if (__DEV__) {
+  const { scriptURL } = NativeModules.SourceCode;
+  // eslint-disable-next-line prefer-destructuring
+  scriptHostname = scriptURL.split('://')[1].split(':')[0];
+}
+
+// if we're running in DEV mode, then let's connect!
+if (__DEV__) {
+  Reactotron.setAsyncStorageHandler(AsyncStorage);
+  if (scriptHostname) {
+    Reactotron.configure({ host: scriptHostname });
+  }
+  Reactotron.connect();
+  Reactotron.clear();
+}
+
+export default Reactotron;
---
+++ Readme.md
@@ -0,0 +1,13 @@
+Fine Me - App
+
+#1 Stg Simulator
+
+- Android:
+
+1. npm start
+2. npm run android:stg
+
+- iOS:
+
+1. npm start
+2. npm run ios:stg or npm run ios:simulator stg "iPad Air (5th generation)" => what ever you want.
