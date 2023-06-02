7a221ddaf50e7851b7fb75977fb43bd5c5a3037e
HaYeong <hy78733@gmail.com>
2023-06-02 19:13:58 +0900

    Feat: useKakaoLogin hook

---
+++ src/hooks/useKakaoLogin.ts
@@ -0,0 +1,43 @@
+import * as Kakao from '@react-native-seoul/kakao-login';
+
+export default function useKakaoLogin() {
+  //TODO: 에러 핸들링
+  const login = async () => {
+    try {
+      const token = await Kakao.login();
+      return token;
+    } catch (err) {
+      console.log('🔸 → file: useKakao.ts:12 → login → err:', err);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      const message = await Kakao.logout();
+      return message;
+    } catch (err) {
+      console.log('🔸 → file: useKakao.ts:21 → logout → err:', err);
+    }
+  };
+
+  const getProfile = async () => {
+    try {
+      const profile = await Kakao.getProfile();
+      // TODO: 필요한 데이터가 없을 경우 처리
+      return profile;
+    } catch (err) {
+      console.log('🔸 → file: useKakao.ts:30 → getProfile → err:', err);
+    }
+  };
+
+  const unlink = async () => {
+    try {
+      const message = await Kakao.unlink();
+      return message;
+    } catch (err) {
+      console.log('🔸 → file: useKakao.ts:39 → unlink → err:', err);
+    }
+  };
+
+  return { login, logout, getProfile, unlink };
+}
