c70b9b4a6b23ddd2079b95538d2478ec36c84730
wglee0511 <ranazang@naver.com>
2023-06-17 10:57:31 +0900

    feat: 라디오 버튼 컴포넌트 구현

--- src/screens/landing/LandingScreen.tsx
+++ src/screens/landing/LandingScreen.tsx
@@ -6,6 +6,7 @@ import { TEXT_COLORS } from '@/themes/colors';
 import { ScaledSheet } from '@/utils/scale';
 import { AppleLoginButton } from '@/features/Landing/Components';
 import { isIOS } from '@/utils/device';
+import { Radio } from '@/common/components/Radio';
 
 const styles = ScaledSheet.create({
   container: {
@@ -19,6 +20,10 @@ const LandingScreen = () => {
       <Text fontSize="28" fontWeight="Regular" color={TEXT_COLORS.textPrimary}>
         랜딩 페이지
       </Text>
+      <Radio onPress={() => {}} isClicked={false} />
+      <Radio onPress={() => {}} isClicked={true} />
+      <Radio onPress={() => {}} isClicked={false} disabled />
+      <Radio onPress={() => {}} isClicked={true} disabled />
       {isIOS && <AppleLoginButton />}
     </View>
   );
