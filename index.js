f0209fb9226c8bc228f6068138eb6684f6a4fa5c
HaYeong <hy78733@gmail.com>
2023-07-01 00:17:13 +0900

    fix: constants url error

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 import { registerBackgroundFCMHandler } from './src/utils/fcm/messageHandlers';
-import { PRODUCTION_TYPE } from '@/utils/constants';
+import { PRODUCTION_TYPE } from '@/constants/config';
 
 // ios에서 백그라운드 알림을 받았을 시 루트 리액트 컴포넌트를 마운트하지 않는다
 function HeadlessCheck({ isHeadless }) {
