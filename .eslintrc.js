8b33b10b6dc920b506afa9c15d84b30b5e39277a
HaYeong <hy78733@gmail.com>
2023-07-07 22:15:21 +0900

    setting: eslint, code formatting 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   root: true,
-  extends: [
-    '@react-native-community',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['@react-native-community', 'prettier'],
+  plugins: ['prettier'],
   rules: {
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
     'prettier/prettier': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react-native/no-inline-styles': 'off',
   },
 };
