d7da4f98c5b18dbe2d27782b6c7071e71fe24c8b
wglee0511 <ranazang@naver.com>
2023-07-18 00:41:18 +0900

    chore: 에러수정

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-/**
- * @format
- */
-
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 import { registerBackgroundFCMHandler } from './src/utils/fcm/messageHandlers';
-import { PRODUCTION_TYPE } from '@/utils/constants';
+import { PRODUCTION_TYPE } from './src/constants/config';
 import 'react-native-get-random-values';
 
 // ios에서 백그라운드 알림을 받았을 시 루트 리액트 컴포넌트를 마운트하지 않는다
