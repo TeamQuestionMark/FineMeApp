206eb6a5c2c6b1087a8739eade8be9875e91ccc9
HaYeong <hy78733@gmail.com>
2023-07-18 20:16:35 +0900

    feat: useNotifications query hook

--- src/api/Notification/api.ts
+++ src/api/Notification/api.ts
@@ -2,8 +2,15 @@ import { mainAxios } from '../shared/axios';
 import { ResponseData } from '../shared/type';
 import { NotificationListData } from './types';
 
+export const queryKey = {
+  all: 'notification' as const,
+  lists: () => [queryKey.all, 'lists'],
+};
+
 export const getNotificationList = async () => {
-  return await mainAxios.get<ResponseData<NotificationListData>>(
+  const response = await mainAxios.get<ResponseData<NotificationListData>>(
     `/api/v1/notification`,
   );
+
+  return response.data.data;
 };
