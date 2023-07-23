957dbbdfcc6fc00af4d863d930f06ec336ce1bfc
HaYeong Jeong <hy78733@gmail.com>
2023-07-23 14:38:58 +0900

    feat: 사용자 이용약관, 개인정보 처리방침 웹뷰 연결 (#42)

--- src/screens/setting/PrivacyPolicyScreen.tsx
+++ src/screens/setting/PrivacyPolicyScreen.tsx
@@ -4,15 +4,24 @@ import { View } from 'react-native';
 import Header from '@/common/components/Header/Header';
 import { useNavigation } from '@react-navigation/native';
 import { NavigationProps } from '@/navigations/types';
+import WebView from 'react-native-webview';
+import { FINEME_WEB_URL } from '@/constants/config';
+import globalStyles from '@/themes/globalStyles';
 
 const PrivacyPolicyScreen = () => {
   const navigation = useNavigation<NavigationProps>();
   return (
-    <View>
+    <View
+      style={[
+        globalStyles.defaultFlexContainer,
+        globalStyles.defaultBackgroundColor,
+      ]}
+    >
       <Header
         title="개인정보 처리방침"
         onPressLeadingIcon={navigation.goBack}
       />
+      <WebView source={{ uri: FINEME_WEB_URL + '/terms/privacy' }} />
     </View>
   );
 };
--- src/screens/setting/TermsScreen.tsx
+++ src/screens/setting/TermsScreen.tsx
@@ -4,12 +4,22 @@ import { View } from 'react-native';
 import Header from '@/common/components/Header/Header';
 import { useNavigation } from '@react-navigation/native';
 import { NavigationProps } from '@/navigations/types';
+import { FINEME_WEB_URL } from '@/constants/config';
+import WebView from 'react-native-webview';
+import globalStyles from '@/themes/globalStyles';
 
 const TermsScreen = () => {
   const navigation = useNavigation<NavigationProps>();
+  console.log(FINEME_WEB_URL);
   return (
-    <View>
+    <View
+      style={[
+        globalStyles.defaultFlexContainer,
+        globalStyles.defaultBackgroundColor,
+      ]}
+    >
       <Header title="사용자 이용약관" onPressLeadingIcon={navigation.goBack} />
+      <WebView source={{ uri: FINEME_WEB_URL + '/terms/service' }} />
     </View>
   );
 };
